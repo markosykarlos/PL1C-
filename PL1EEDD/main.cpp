97b8ef15e035277020e732a649b0114a50dc4b8f
markosykarlos <markosjimmm@gmail.com>
2025-10-14 12:53:40 +0200

    C++
    Co-Authored-By: Riverfan <197075022+riverfan2018@users.noreply.github.com>

--- PL1EEDD/main.cpp
+++ PL1EEDD/main.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+void ImprimirMenu(void)
+{
+    cout << "=== MENU ===" << endl;
+    cout << "1) Generar N pedidos" << endl;
+    cout << "2) Paso (Una FASE)" << endl;
+    cout << "3) Mostrar estado" << endl;
+    cout << "4) Paso COMPLETO (4fases)" << endl;
+    cout << "5) Ver caja de una libreria" << endl;
+    cout << "0) Salir\n" << endl;
+    cout << "Opcion: ";
+}
+
+struct Alumno
+{
+    string id_editorial;
+    string id_pedido;
+    string cod_libro;
+    string materia;
+    string estado;
+    int unidades;
+};
+
 int main()
 {
     return 0;
