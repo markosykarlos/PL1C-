1a41a7045e2bc75fe6d4a1c7161706720239bd4a
markosykarlos <markosjimmm@gmail.com>
2025-10-14 13:11:06 +0200

    Update main.cpp
    Co-Authored-By: Riverfan <197075022+riverfan2018@users.noreply.github.com>

--- PL1EEDD/main.cpp
+++ PL1EEDD/main.cpp
@@ -14,7 +14,7 @@ void ImprimirMenu(void)
     cout << "Opcion: ";
 }
 
-struct Alumno
+struct pedido
 {
     string id_editorial;
     string id_pedido;
@@ -23,6 +23,107 @@ struct Alumno
     string estado;
     int unidades;
 };
+
+class Nodo
+{
+private:
+    pedido valor;
+    Nodo *siguiente;
+    friend class Cola;
+
+
+public:
+    Nodo(pedido v, Nodo *sig = NULL)
+    {
+        valor = v;
+        siguiente = sig;
+    }
+};
+typedef Nodo *pNodo;
+
+class Cola
+{
+private:
+    pNodo primero, ultimo;
+
+public:
+    Cola() : primero(NULL), ultimo(NULL) {}
+    ~Cola();
+    void encolar(pedido v);
+    int desencolar();
+};
+
+// Destructor
+Cola::~Cola()
+{
+    while (primero)
+        desencolar();
+}
+
+// Encolar (añadir al final)
+void Cola::encolar(pedido v)
+{
+    pNodo nuevo = new Nodo(v);
+    if (ultimo)
+        ultimo->siguiente = nuevo;
+    ultimo = nuevo;
+    if (!primero)
+        primero = nuevo;
+}
+
+// Desencolar (sacar del frente)
+pedido Cola::desencolar()
+{
+    if (!primero)
+        return {"", "", "", "", "", 0};
+
+    int v = primero->valor;
+    pNodo nodo = primero;
+    primero = nodo->siguiente;
+    if (!primero)
+        ultimo = NULL;
+    delete nodo;
+    return v;
+}
+
+class Pila
+{
+private:
+    pNodo cima;
+
+public:
+    Pila() : cima(NULL) {} // Constructor
+    ~Pila();
+    void apilar(pedido v);
+    pedido desapilar();
+};
+
+// Destructor
+Pila::~Pila()
+{
+    while (cima)
+        desapilar();
+}
+
+// Apilar
+void Pila::apilar(pedido v)
+{
+    pNodo nuevo = new Nodo(v, cima);
+    cima = nuevo;
+}
+
+// Desapilar
+pedido Pila::desapilar()
+{
+    if (!cima)
+        return {"", 0};
+
+    pNodo nodo = cima;
+    pedido v = nodo->valor;
+    cima = nodo->siguiente;
+    delete nodo;
+    return v;
+}
 
 int main()
 {
