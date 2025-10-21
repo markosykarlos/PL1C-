0d3ba15f103f7df10bfd84d77428a85d74225f6f
markosykarlos <markosjimmm@gmail.com>
2025-10-21 17:15:45 +0200

    stock

--- PL1EEDD/Editorial.cpp
+++ PL1EEDD/Editorial.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 Cola colaIniciado, colaAlmacen, colaImprenta, colaListo;
 Pila cajas[LIBRERIAS];
+StockLibro stock[MAX_TITULOS];
 NodoStock* stockPun = NULL;
 
 // Destructor
@@ -104,16 +105,52 @@ void ImprimirMenu(void)
     cout << "Opcion: ";
 }
 
-int calcularSecuenciaAleatoria(void)
+//int calcularSecuenciaAleatoria(void)
+//{
+  //  int numaleat;
+    //numaleat=rand()%6;
+   // return numaleat;
+//}
+
+//int calcularSecuenciaAleatoria2(void)
+//{
+  //  return 10000 + rand() % 90000;
+//}
+
+// Genera un código de libro al azar
+string GenerarCodLibro(void)
 {
     int numaleat;
-    numaleat=rand()%6;
-    return numaleat;
+    int numaleat2;
+    int indaleat;
+    char letras[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    numaleat=rand()%1000; // Habria que cambia para que genere 000 en vez de 0 en el menor de los casos, así siempre tendría 3 num + 1 letra + 2 num
+    indaleat=rand()%26;
+    numaleat2=rand()%100; // igual a arriba
+    return to_string(numaleat) + letras[indaleat] + to_string(numaleat2);
 }
 
-int calcularSecuenciaAleatoria2(void)
-{
-    return 10000 + rand() % 90000;
+// Genera una materia aleatoria
+string GenerarMateria(){
+    int numaleat;
+    numaleat=rand()%6;
+    string Materias[] = {"Matematicas","Fisica","Tecnologia","Musica","Historia","Lengua"};
+    return Materias[numaleat];
+}
+
+StockLibro GenerarStock(){
+    StockLibro st;
+    st.cod_libro = GenerarCodLibro();
+    st.materia = GenerarMateria();
+    st.unidades = rand()%26;
+    return st;
+}
+
+void CrearStock(){
+    int i;
+    for(i=0; i<MAX_TITULOS; i++){
+        stock[i] = GenerarStock();
+    }
 }
 
 // Función complementaria de MostrarEstadoSistema()
@@ -202,27 +239,6 @@ string GenerarNumPedido(void)
     return "P" + to_string(numaleat);
 }
 
-// Genera un código de libro al azar
-string GenerarCodLibro(void)
-{
-    int numaleat;
-    int numaleat2;
-    int indaleat;
-    char letras[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    numaleat=rand()%1000; // Habria que cambia para que genere 000 en vez de 0 en el menor de los casos, así siempre tendría 3 num + 1 letra + 2 num
-    indaleat=rand()%26;
-    numaleat2=rand()%100; // igual a arriba
-    return to_string(numaleat) + letras[indaleat] + to_string(numaleat2);
-}
-
-// Genera una materia aleatoria
-string GenerarMateria(){
-    int numaleat;
-    numaleat=rand()%6;
-    string Materias[] = {"Matematicas","Fisica","Tecnologia","Musica","Historia","Lengua"};
-    return Materias[numaleat];
-}
-
 // Genera una unidad aleatoria
 int GenerarUnidades(){
     int numaleat;
@@ -240,8 +256,8 @@ string GenerarEstado(int indice){
 pedido GenerarPedido(){
     int lib = GenerarLib();
     string id = GenerarNumPedido();
-    string codigo = GenerarCodLibro();
-    string materia = GenerarMateria();
+    //string codigo = GenerarCodLibro();
+    //string materia = GenerarMateria();
     int unidades = GenerarUnidades();
     string estado = GenerarEstado(0);
     pedido p = {lib, id, codigo, materia, unidades, estado};
